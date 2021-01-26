4747180b2b5474b9a325760e0a042f044bbded3b
reinan martins <reinan1020@outlook.com>
2021-01-26 20:20:24 -0300

    1

---
+++ bd.sql
@@ -0,0 +1,14 @@
+create database bd;
+#drop database bd;
+use bd;
+
+Create table usuarios (
+ID Int UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
+login Varchar(30),
+senha Varchar(40),
+nome varchar(55),
+cidade varchar(40),
+numero_celular fixed(00000000000),
+
+Primary Key (ID)) ENGINE = MyISAM;
+select * from usuarios;
