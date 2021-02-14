3e51cfa9db57e46f03f56855806b396f857f4f93
pauloJ34 <paulofranklin612@gmail.com>
2021-02-14 17:01:42 -0300

    passa de imagem

--- JAVASCRIPT/interation.js
+++ JAVASCRIPT/interation.js
@@ -2,6 +2,47 @@ window.onload=function(){
     function invisivel(){
         try{
         document.querySelector("#erro").remove()
-        }catch(e){console.log("erro, ID erro não encontrado")}
+        }catch(e){}
     }setTimeout(invisivel,6*1000)
+    var conta=0;
+    var n=0;
+    num=document.querySelectorAll(".anunciototal").length
+    document.querySelector("#passar").addEventListener("click",function(){
+        setTimeout(ver,0)  
+        n++     
+        //console.log(n)
+        conta-=100
+        //console.log(conta)
+        for(var i=0; i<num ;i++){
+            //console.log(document.querySelectorAll(".anunciototal")[i])
+            document.querySelectorAll(".anunciototal")[i].style.left=conta+"%"
+        }
+        //console.log(document.querySelectorAll(".anunciototal").length)
+        //console.log(document.querySelectorAll(".anunciototal"))
+        function ver(){
+            if(n<(num-3)){document.querySelector("#passar").style.display="block"
+            document.querySelector("#voltar").style.display="block"
+        }
+            else{document.querySelector("#passar").style.display="none" }
+        }setTimeout(ver,200)
+        
+    })
+    document.querySelector("#voltar").addEventListener("click",function(){
+        setTimeout(ver,0)
+        //console.log(n)
+        n--
+        conta+=100
+        //console.log(conta)
+        //console.log(document.querySelectorAll(".anunciototal").length)
+        //console.log(document.querySelectorAll(".anunciototal"))
+        for(var i=0; i<num ;i++){
+            //console.log(document.querySelectorAll(".anunciototal")[i])
+            document.querySelectorAll(".anunciototal")[i].style.left=conta+"%"
+        } 
+        function ver(){
+            if(n!==0){document.querySelector("#voltar").style.display="block"
+            document.querySelector("#passar").style.display="block"}
+            else{document.querySelector("#voltar").style.display="none" }
+        }setTimeout(ver,200)
+    })
 }
--- index.php
+++ index.php
@@ -14,6 +14,7 @@
     <title>PotiCars- Um Mercado Virtual Potiguar de Vendas Automotiva</title>
     <link rel="stylesheet" href="CSS/stylePrincipal.css">
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <script src="JAVASCRIPT/interation.js"></script>
 </head>
 </head>
 <body>
@@ -24,32 +25,34 @@
     ?>
     
 	<div class="meio">
-        	<div class="anunciototal">
-            		<div class="anuncio"><img src="https://i.vrum.com.br/JyKahLylgVfhOkEFuZfxB8W03NQ=/675x/smart/imgsapp.estadodeminas.vrum.com.br/app/noticia_128576568202/2020/09/16/55316/20200915164159518844e.jpg" alt=""></div>
-			<div class="descricao">DESCRIÇÃO</div>
-        	</div>
-          	<div class="anunciototal">
-            		<div class="anuncio"><img src="https://i.vrum.com.br/JyKahLylgVfhOkEFuZfxB8W03NQ=/675x/smart/imgsapp.estadodeminas.vrum.com.br/app/noticia_128576568202/2020/09/16/55316/20200915164159518844e.jpg" alt=""></div>
-			<div class="descricao">DESCRIÇÃO</div>
-        	</div>
-  		<div class="anunciototal">
-            		<div class="anuncio"><img src="https://i.vrum.com.br/JyKahLylgVfhOkEFuZfxB8W03NQ=/675x/smart/imgsapp.estadodeminas.vrum.com.br/app/noticia_128576568202/2020/09/16/55316/20200915164159518844e.jpg" alt=""></div>
-			<div class="descricao">DESCRIÇÃO</div>
-        	</div>
-    </div>
-    <div class="meio">
-        	<div class="anunciototal">
-            		<div class="anuncio"><img src="https://i.vrum.com.br/JyKahLylgVfhOkEFuZfxB8W03NQ=/675x/smart/imgsapp.estadodeminas.vrum.com.br/app/noticia_128576568202/2020/09/16/55316/20200915164159518844e.jpg" alt=""></div>
-			<div class="descricao">DESCRIÇÃO</div>
-        	</div>
-          	<div class="anunciototal">
-            		<div class="anuncio"><img src="https://i.vrum.com.br/JyKahLylgVfhOkEFuZfxB8W03NQ=/675x/smart/imgsapp.estadodeminas.vrum.com.br/app/noticia_128576568202/2020/09/16/55316/20200915164159518844e.jpg" alt=""></div>
-			<div class="descricao">DESCRIÇÃO</div>
-        	</div>
-  		<div class="anunciototal">
-            		<div class="anuncio"><img src="https://i.vrum.com.br/JyKahLylgVfhOkEFuZfxB8W03NQ=/675x/smart/imgsapp.estadodeminas.vrum.com.br/app/noticia_128576568202/2020/09/16/55316/20200915164159518844e.jpg" alt=""></div>
-			<div class="descricao">DESCRIÇÃO</div>
-        	</div>
+        <div class="ads">
+            <div class="anunciototal">
+                <div class="anuncio"><img src="https://i.vrum.com.br/JyKahLylgVfhOkEFuZfxB8W03NQ=/675x/smart/imgsapp.estadodeminas.vrum.com.br/app/noticia_128576568202/2020/09/16/55316/20200915164159518844e.jpg" alt=""></div>
+            <div class="descricao">DESCRIÇÃO 1</div>
+            </div>
+            <div class="anunciototal">
+                <div class="anuncio"><img src="https://i.vrum.com.br/JyKahLylgVfhOkEFuZfxB8W03NQ=/675x/smart/imgsapp.estadodeminas.vrum.com.br/app/noticia_128576568202/2020/09/16/55316/20200915164159518844e.jpg" alt=""></div>
+            <div class="descricao">DESCRIÇÃO 2</div>
+            </div>
+            <div class="anunciototal">
+                <div class="anuncio"><img src="https://i.vrum.com.br/JyKahLylgVfhOkEFuZfxB8W03NQ=/675x/smart/imgsapp.estadodeminas.vrum.com.br/app/noticia_128576568202/2020/09/16/55316/20200915164159518844e.jpg" alt=""></div>
+            <div class="descricao">DESCRIÇÃO 3</div>
+            </div>
+            <div class="anunciototal">
+                <div class="anuncio"><img src="https://i.vrum.com.br/JyKahLylgVfhOkEFuZfxB8W03NQ=/675x/smart/imgsapp.estadodeminas.vrum.com.br/app/noticia_128576568202/2020/09/16/55316/20200915164159518844e.jpg" alt=""></div>
+            <div class="descricao">DESCRIÇÃO 4</div>
+            </div>
+            <div class="anunciototal">
+                <div class="anuncio"><img src="https://i.vrum.com.br/JyKahLylgVfhOkEFuZfxB8W03NQ=/675x/smart/imgsapp.estadodeminas.vrum.com.br/app/noticia_128576568202/2020/09/16/55316/20200915164159518844e.jpg" alt=""></div>
+            <div class="descricao">DESCRIÇÃO 5</div>
+            </div>
+            <div class="anunciototal">
+                <div class="anuncio"><img src="https://i.vrum.com.br/JyKahLylgVfhOkEFuZfxB8W03NQ=/675x/smart/imgsapp.estadodeminas.vrum.com.br/app/noticia_128576568202/2020/09/16/55316/20200915164159518844e.jpg" alt=""></div>
+            <div class="descricao">DESCRIÇÃO 6</div>
+            </div>
+        </div>
+        <div class='seta voltar' id="voltar"><i></i></div>
+        <div class='seta passar' id="passar"><i></i></div>
     </div>
     <p style='text-align:center' >1,2,3,4,5...</p>
 	<div class="rodape">
