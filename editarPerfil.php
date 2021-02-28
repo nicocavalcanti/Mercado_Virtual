e5b812eb71b20d5413961ed45af23e4c540ddb7b
pauloJ34 <paulofranklin612@gmail.com>
2021-02-28 19:16:39 -0300

    edita perfil

--- editarPerfil.php
+++ editarPerfil.php
@@ -1,83 +1,55 @@
-<!--<?php
-   
-   error_reporting(0);
-   session_start();
-   include_once("BACKEND/connection.php");
-   $_SESSION["pagina"]="register";
-   include("BACKEND/verificar/verific.php");
-   //print_r(__FILE__);
-   //pathinfo(__FILE__)["filename"]//volta o nome do arquivo
-?>-->
+<?php 
+session_start();
+include_once("BACKEND/connection.php");
+include("BACKEND/verificar/verific.php");
+?>
 <!DOCTYPE html>
 <html lang="PT-BR">
 <head>
-   <meta charset="UTF-8">
-   <link rel="icon" href="./IMG/logo.jpeg" type="image/jpeg">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link rel="stylesheet" href="CSS/styleCadastro.css">
-   <script src="JAVASCRIPT/interation.js"></script>
-
-   <title>Editando Usuário</title>
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./IMG/logo.jpeg" type="image/jpeg">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="CSS/styleMeuPerfil.css">
+    <title>Perfil de Usuario</title>
 </head>
 <body>
-   <main class="container">  
-      <a href="login.php"><img src="IMG/36976.png" alt="" width=25></a>
-      <h2>Editando Perfil...</h2>
-      <form action="BACKEND/registrar/registerLogin" method="post">
-
-         <div class="input-field">
-            <input type="text" name="completName" placeholder="Nome completo">
-            <div class="underline"></div>
-         </div>
-         <div class="input-field">
-            <input type="text" name="nickName" placeholder="Apelido">
-            <div class="underline"></div>
-         </div>
-         <div class="input-field">
-            <input type="text" name="city" placeholder="Sua cidade">
-            <div class="underline"></div>
-         </div>
-         <div class="input-field">
-            <input type="text" name="contact" placeholder="Contato">
-            <div class="underline"></div>
-         </div>
-         <div class="input-field">
-            <input type="text" name="email" placeholder="E-mail">
-            <div class="underline"></div>
-         </div>
-         <div class="input-field">
-            <input type="password" name="password" placeholder="Senha">
-            <div class="underline"></div>
-         </div>
-         
-         <input type="submit" value="Salvar">
-      </form>
-      
-   </main>
-      <!--<?php
-      /*
-      esse trecho de php serve para aparecer a messagem de erro ou de cadastro
-      */
-         //$_SESSION['vaziu']=true;
-         //$_SESSION['cadastrado']=true;
-         //$_SESSION["jaCadastrado"]=true;
-         if (isset($_SESSION["vaziu"])) {
-            echo " <div class='erro' id='erro'>
-                     <p><strong>Campus vaziu!</strong><br> por favor preencha todos os campus.</p>
-                  </div>";
-            unset($_SESSION["vaziu"]);
-         }elseif(isset($_SESSION['cadastrado'])){
-            echo" <div class='erro cadastrado' id='erro'>
-                     <p><strong>Cadastrado Com Sucesso!</strong></p>
-                  </div>";
-            unset($_SESSION['cadastrado']);
-         }elseif(isset($_SESSION["jaCadastrado"])) {
-            echo " <div class='erro' id='erro'>
-                     <p><strong>Erro!</strong><br> Usuário já cadastrado.</p>
-                  </div>";
-            unset($_SESSION["jaCadastrado"]);
-         }
-      ?>-->
+    <div class="tudo">
+        <?php
+            include("header/header.php");
+        ?>        
+            <main class="container">
+               <form action="" method="get">
+            <!--Caso não esteja logado, Fazer Login -  Colocar um IF sla-->
+                  <?php 
+                  
+                  $nome=$pdo->query("SELECT * FROM usuarios WHERE apelido_usuario='".$_SESSION["login"]."';")->fetch(PDO::FETCH_OBJ);
+                  ?>
+               
+                  <h2>Meus Dados</h2>
+                  <table>
+                  <tr>
+                     <td><p><strong>Nome: </strong></p></td> 
+                     <td><p><input type="text" value="<?php echo $nome->nome;?>"></p></td>
+                  </tr>
+                  <tr>
+                     <td><p><strong>cidade: </strong></p> </td> 
+                     <td><p><input type="text" value="<?php echo $nome->cidade;?>"></p></td>
+                  </tr>
+                  <tr>
+                     <td><p><strong>Contato: </strong></p> </td> 
+                     <td><p><input type="text" value="<?php echo $nome->numero_celular;?>"></p></td>
+                  </tr>
+                  <tr>
+                     <td><p><strong>E-Mail: </strong></p> </td> 
+                     <td><p><input type="text" value="<?php echo $nome->email;?>"></p></td>
+                  </tr>
+                  </table>
+                  <input type="submit" value="enviar" class="enviar">
+                  
+               </form>    
+        </main>
+    </div>  
+    
 </body>
 </html>
--- perfil.php
+++ perfil.php
@@ -7,7 +7,6 @@ include("BACKEND/verificar/verific.php");
 <html lang="PT-BR">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="./IMG/logo.jpeg" type="image/jpeg">
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
@@ -15,12 +14,10 @@ include("BACKEND/verificar/verific.php");
     <title>Perfil de Usuario</title>
 </head>
 <body>
-    
+    <div class="tudo">
         <?php
             include("header/header.php");
-        ?>
-        
-        
+        ?>        
             <main class="container">
             
             <!--Caso não esteja logado, Fazer Login -  Colocar um IF sla-->
@@ -30,19 +27,32 @@ include("BACKEND/verificar/verific.php");
                 ?>
                
                 <h2>Meus Dados</h2>
-                        <p>Nome: <?php echo $nome->nome;?></p>
-                        <p>cidade: <?php echo $nome->cidade;?></p>
-                        <p>Contato: <?php echo $nome->numero_celular;?></p>
-                        <p>E-Mail: <?php echo $nome->email;?></p>
+                <table>
+                  <tr>
+                     <td><p><strong>Nome: </strong></p></td> 
+                     <td><p><?php echo $nome->nome;?></p></td>
+                  </tr>
+                  <tr>
+                     <td><p><strong>cidade: </strong></p> </td> 
+                     <td><p><?php echo $nome->cidade;?></p></td>
+                  </tr>
+                  <tr>
+                     <td><p><strong>Contato: </strong></p> </td> 
+                     <td><p><?php echo $nome->numero_celular;?></p></td>
+                  </tr>
+                  <tr>
+                     <td><p><strong>E-Mail: </strong></p> </td> 
+                     <td><p><?php echo $nome->email;?></p></td>
+                  </tr>
+                  </table>
                   
                     <h2 >Editar Meu Perfil</h2>
                    <a href="editarPerfil">Clique aqui e edite seu Perfil</a>
                
                     <h2 >Meus Anúncios</h2>
                  <a href="meus_anuncios" target="_blank" >Clique aqui para ver seus anúncios</a>
-       
         </main>
-       
+    </div>
 
     
         
