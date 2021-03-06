9b258339f912cf50df63a3aa300b4e4f83c37f4f
pauloJ34 <paulofranklin612@gmail.com>
2021-03-06 15:52:44 -0300

    add

--- sobre.php
+++ sobre.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS/styleContato.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
     <title>Sobre</title>
 </head>
 <body>
@@ -12,19 +13,19 @@
         <?php
         include("header/header.php");
         ?>
-    <div class="meio">
-       
-        <p style="text-align: center;">
-            <p style="text-align: center;"> Site desenvolvido para fins acadêmicos </p>
-                Mais especificamente para a disciplina de PI - Programação para a Internet do curso de tecnico integrado em Informatica do IFRN - Campus Nova Cruz.
+        <div class="meio">
         
-        </p>
-    </div>
-    <div class="rodape">
-        <p style='text-align:center' >Feito por Luiz, Paulo e Reinan</p>
-        <p style='text-align:center' >Copyright © 2021-2021 PotiCars.com.br LTDA.</p>
+            <p style="text-align: center;">
+                <p style="text-align: center;"> Site desenvolvido para fins acadêmicos </p>
+                    Mais especificamente para a disciplina de PI - Programação para a Internet do curso de tecnico integrado em Informatica do IFRN - Campus Nova Cruz.
+            
+            </p>
+        </div>
+        <div class="rodape">
+            <p style='text-align:center' >Feito por Luiz, Paulo e Reinan</p>
+            <p style='text-align:center' >Copyright © 2021-2021 PotiCars.com.br LTDA.</p>
 
-    </div>
+        </div>
     </div>
     
 </body>
